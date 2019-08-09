76d6e9f917eda5713b9e6a2823ac844058accc2d
Mohan Kumar <46491614+imshines@users.noreply.github.com>
2019-08-09 10:16:02 +0530

    First Commit

---
+++ js/script.js
@@ -0,0 +1,8 @@
+const btnClr = document.querySelector('button');
+
+btnClr.addEventListener('click', () => {
+    let num = "Hey Mohan !";
+    for(let i = 0; i <= 10; i = i+1){
+        console.log(num[i]);
+    }    
+});
